adebd69a63e765a4e46b63a292c2f38a098a0078
HerrNamenlos123 <herrnamenlos123@gmail.com>
2024-04-05 18:22:45 +0200

    Add more logging

--- build.py
+++ build.py
@@ -1,5 +1,6 @@
 import sys
 import importlib
+import scripts.tools as tools
 
 # Usage: python build.py <package_name>
 
@@ -11,22 +12,36 @@ def main():
     package_name = sys.argv[1]
     builder = getattr(importlib.import_module(f'packages.{package_name}.build'), 'Builder')()
 
+    tools.log("Installing build dependencies ...")
     builder.install_build_dependencies()
+    tools.log("Installing build dependencies ... Done")
     
     if hasattr(builder, 'source'):
+        tools.log("Sourcing build files ...")
         builder.source()
+        tools.log("Sourcing build files ... Done")
 
     if hasattr(builder, 'patch_sources'):
+        tools.log("Patching source files ...")
         builder.patch_sources()
+        tools.log("Patching source files ... Done")
 
     if hasattr(builder, 'depends'):
+        tools.log("Fetching dependencies ...")
         builder.depends()
+        tools.log("Fetching dependencies ... Done")
 
     if hasattr(builder, 'build'):
+        tools.log("Building the package ...")
         builder.build()
+        tools.log("Building the package ... Done")
 
     if hasattr(builder, 'package'):
+        tools.log("Packaging build artifacts ...")
         builder.package()
+        tools.log("Packaging build artifacts ... Done")
+        
+    tools.log("Build process is done")
 
 if __name__ == '__main__':
     main()
